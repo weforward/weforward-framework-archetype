23c7b2da89538c4126bb43890ac51566ec42c4da
bossky <bossky8023@gmail.com>
2021-06-10 21:08:36 +0800

    调整代码

--- src/main/resources/archetype-resources/src/test/java/test/HomeTest.java
+++ src/main/resources/archetype-resources/src/test/java/test/HomeTest.java
@@ -54,7 +54,15 @@ public class HomeTest {
 		Response response = getInvoker().invoke("home/hello", params);
 		System.out.println("code:" + response.getResponseCode());
 		System.out.println("msg:" + response.getResponseMsg());
-		System.out.println("result" + response.getServiceResult());
+		System.out.println("result:" + response.getServiceResult());
+		synchronized (this) {
+			try {
+				this.wait(10 * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("done");
 	}
 
 }
